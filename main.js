74a6b77237842e7e8828d3c91f9d07d463e162a3
@andriy.trofimov <trofimov.a@hanaboso.com>
2021-07-28 20:34:12 +0300

    Load index.html file

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 const { app, BrowserWindow } = require('electron');
 
+let mainWindow;
+
 function createMainWindow()
 {
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     title: 'Image Shrink',
     width: 500,
     height: 600,
+    icon: './assets/icons/Icon_256x256.png'
   })
+
+  mainWindow.loadFile(`./app/index.html`)
 }
 
 app.on('ready', createMainWindow);
