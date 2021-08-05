9bb082eb0cad8168be5ff2adcde49ed7e5b59e23
Andriy Trofimov <webgrandmo@gmail.com>
2021-08-05 22:40:00 +0300

    Working on menu

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu } = require('electron');
+
+
+process.env.NODE_ENV = 'develpoment';
+
+const isDev = process.env.NODE_ENV !== 'production' ? true : false;
+const isMac = process.platform === 'darwin' ? true : false;
+const isWin = process.platform === 'win32' ? true : false;
 
 let mainWindow;
 
@@ -8,10 +15,48 @@ function createMainWindow()
     title: 'Image Shrink',
     width: 500,
     height: 600,
-    icon: './assets/icons/Icon_256x256.png'
+    icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+    resizable: isDev
   })
 
   mainWindow.loadFile(`./app/index.html`)
 }
 
-app.on('ready', createMainWindow);
+app.on('window-all-closed', function ()
+{
+  if (!isMac) app.quit()
+})
+
+app.whenReady().then(() =>
+{
+  createMainWindow()
+
+  const mainMenu = Menu.buildFromTemplate(menu);
+  Menu.setApplicationMenu(mainMenu);
+
+  app.on('activate', function ()
+  {
+    if (BrowserWindow.getAllWindows().length === 0) createMainWindow()
+  })
+})
+
+const menu = [
+  ...(isMac ? [{
+    role: 'appMenu'
+  }]: []),
+  {
+    label: "File",
+    submenu: [
+      {
+        label: "Quit",
+        click: () => app.quit()
+      }
+    ]
+  }
+]
+
+app.on('ready', () =>
+{
+  createMainWindow()
+  mainWindow.on('close', () => mainWindow = null)
+});
